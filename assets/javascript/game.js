0ade86660abc226d6d4299d8c7e672e2123a774b
Chris Laughter <chrislaugh@gmail.com>
2019-09-12 13:15:12 -0600

    work in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -98,7 +98,7 @@ $(document).ready(function() {
 	// Functions****
 
 	// pick a random number
-	function pickRandomNumber(arr) {
+	function randNum(num) {
 
 		var z = num[Math.floor(Math.random() * num.length)];
 		randNumber = z;
@@ -106,105 +106,7 @@ $(document).ready(function() {
 
 		console.log('randNumber');
 
-	} // END of pickRandomNumber function
-
-	// // pick random numbers for crystals
-
-	// function pickRandomCrystals(arr) {
-
-	// 	for (var y = 0; y < 4; y++){
-
-	// 		var a = arr[Math.floor(Math.random() * arr.length)];
-
-	// 		crystalNumbers.push(a);
-	// 	}
-  //   // check which numbers have been picked
-	// 	console.log("crystal numbers: " + crystalNumbers);
-
-	// } // END of pickRandomCrystals function
-
-	// function crystalValues(arr) {
-
-	// 	// change value of each crystal button according to array
-	// 	for (i = 0; i < arr.length; i++) {
-
-	// 	$("#button-" + (i+1)).attr("value", arr[i]);
-	// 	console.log(this);
-	// 	}
-	// 	c1 = arr[0];
-	// 	c2 = arr[1];
-	// 	c3 = arr[2];
-	// 	c4 = arr[3];
-	// } // END of crystalValues function
-
-	// function gameReset(x) {
-
-	// 	crystalNumbers = []; // clears crystal number values
-
-	// 	pickRandomNumber(rand);
-
-	// 	pickRandomCrystals(crystals);
-
-	// 	crystalValues(crystalNumbers);
-
-	// 	totalScore = 0;
-	// 	$("#totalNumber").html(totalScore);
-
-	// 	alert(x);
-	// } // END of gameReset function
-
-	// // *** GAME SETTINGS AT START ***
-
-	// pickRandomNumber(rand); // random number to match
-	// pickRandomCrystals(crystals); // array of random crystal values
-	// crystalValues(crystalNumbers);
-
-	// 	// crystal button functions
-
-	// 	$("#button-1").on("click", function() {
-
-	// 		totalScore += c1;
-	// 		$("#totalNumber").html(totalScore);
-	// 	});
-
-	// 	$("#button-2").on("click", function() {
-
-	// 		totalScore += c2;
-	// 		$("#totalNumber").html(totalScore);
-	// 	});
-
-	// 	$("#button-3").on("click", function() {
-
-	// 		totalScore += c3;
-	// 		$("#totalNumber").html(totalScore);
-	// 	});
-
-	// 	$("#button-4").on("click", function() {
-
-	// 		totalScore += c4;
-	// 		$("#totalNumber").html(totalScore);
-	// 	});
-
-	// $("button").on("click", function() {
-	// 	// this is what happens if the user wins
-	// 	if (totalScore == randNumber) {
-
-	// 		wins++;
-	// 		console.log(totalScore);
-	// 		$("#totalNumber").html(totalScore);
-	// 		$("#wins").html("Wins: " + wins);
+	} // End function
 
 
-	// 		setTimeout(function() {gameReset("YOU WIN!!")}, 200);
-	// 	}
-
-	// 	else if (totalScore > randNumber){
-
-	// 		losses++;
-	// 		$("#totalNumber").html(totalScore);
-	// 		$("#losses").html("Losses: " + losses);
-
-	// 		setTimeout(function() {gameReset("WOMP-WOMP...YOU LOSE!")}, 200);
-	// 	}
-	// });
 });
